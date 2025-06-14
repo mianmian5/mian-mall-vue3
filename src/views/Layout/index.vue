4000580901c5ea3d4e8f2f54afa1b801d8fe420e
mian <tianla0615@163.com>
2025-06-14 15:46:34 +0800

    layout-吸顶导航

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,9 +2,11 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoytFixed from './components/LayoytFixed.vue'
 </script>
 
 <template>
+  <LayoytFixed/>
   <LayoutNav />
   <LayoutHeader />
   <RouterView />

f4254a1761de1a8507a3599be552f3be8ec4c8c7
mian <tianla0615@163.com>
2025-06-19 21:07:07 +0800

    将懒加载封装为插件，减少main.js里面的逻辑

---
+++ src/directives/index.js
@@ -0,0 +1,32 @@
+//定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const lazyPlugin = {
+  install(app){
+    // 定义全局指令
+    app.directive('pic-lazy',{
+      mounted(el,binding){
+        //el:指令绑定的元素
+        //binding：指令对象
+        console.log(el,binding)
+        // 监听元素是否进入视口
+        useIntersectionObserver(
+          el,
+          // 回调函数，isIntersecting 表示是否进入视口
+          ([{ isIntersecting }]) => {
+            if (isIntersecting) {
+              // console.log('元素进入视口啦！')
+              // 这里可以写进入视口后的逻辑，比如加载图片、统计曝光等
+              // stop() // 如果只想监听一次，进入视口后可以停止监听
+              el.src = binding.value
+              console.log(isIntersecting)
+            }
+          },
+          // 可选配置：设置进入视口的“占比”阈值（0 ~ 1），默认 0.1（即 10% 进入视口就算）
+          { threshold: 0.01 }
+        )
+          }
+    })
+
+  }
+}
--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { useIntersectionObserver } from '@vueuse/core'
+
+import {lazyPlugin} from '@/directives/index'
 // import { getCategory } from './apis/testApi'
 // getCategory().then(res=>{
 //   console.log('111',res)
@@ -15,30 +16,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')
 
-// 定义全局指令
-app.directive('pic-lazy',{
-  mounted(el,binding){
-    //el:指令绑定的元素
-    //binding：指令对象
-    console.log(el,binding)
-    // 监听元素是否进入视口
-    useIntersectionObserver(
-      el,
-      // 回调函数，isIntersecting 表示是否进入视口
-      ([{ isIntersecting }]) => {
-        if (isIntersecting) {
-          // console.log('元素进入视口啦！')
-          // 这里可以写进入视口后的逻辑，比如加载图片、统计曝光等
-          // stop() // 如果只想监听一次，进入视口后可以停止监听
-          el.src = binding.value
-          console.log(isIntersecting)
-        }
-      },
-      // 可选配置：设置进入视口的“占比”阈值（0 ~ 1），默认 0.1（即 10% 进入视口就算）
-      { threshold: 0.5 }
-    )
-      }
-})
+

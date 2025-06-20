34c0a4f74a5df22f0c22cadf21c6d22220a1525a
mian <tianla0615@163.com>
2025-06-20 10:45:58 +0800

    封装GoodsItem组件

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HomePanel from './HomePanel.vue'
+import GoodsItem from './GoodsItem.vue'
 import {getGoodsAPI} from '@/apis/home'
 import { ref,onMounted } from 'vue'
 const list = ref([])
@@ -26,12 +27,13 @@ onMounted(()=>{
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
+            <GoodsItem :good="good"></GoodsItem>
+            <!-- <RouterLink to="/" class="goods-item">
               <img v-pic-lazy="good.picture" alt="" />
               <p class="name ellipsis">{{ good.name }}</p>
               <p class="desc ellipsis">{{ good.desc }}</p>
               <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+            </RouterLink> -->
           </li>
         </ul>
       </div>

20f1fed4d2f1c719126b162fc91e0033b8bffbb9
mian <tianla0615@163.com>
2025-07-08 23:06:23 +0800

    一级分类-渲染面包屑导航

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -21,7 +21,7 @@
       </h1>
       <ul class="app-header-nav">
         <li class="home" v-for="item in categoryStore.list" :key="item.id">
-          <RouterLink to="/">{{ item.name }}</RouterLink>
+          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
         <!-- <li> <RouterLink to="/">居家</RouterLink> </li>
         <li> <RouterLink to="/">美食</RouterLink> </li>
--- src/views/Layout/components/LayoytFixed.vue
+++ src/views/Layout/components/LayoytFixed.vue
@@ -24,7 +24,7 @@
       <!-- 导航区域 -->
       <ul class="app-header-nav ">
         <li class="home" v-for="item in categoryStore.list" :key="item.id">
-          <RouterLink to="/">{{ item.name }}</RouterLink>
+          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
       </ul>
 

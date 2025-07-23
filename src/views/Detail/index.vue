6ed1a7b70796683588436ac09df508e50e43a511
mian <tianla0615@163.com>
2025-07-23 22:07:01 +0800

    详情界面-封装图片组件-点击小图显示大图【@mouseenter、动态类名控制】

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import { getDetailAPI } from '@/apis/detail';
 import { ref,onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import DetailHot from './components/DetailHot.vue';
+import ImageView  from '@/components/ImageView/index.vue';
 // import DetailHot from './components/DetailHot.vue';
 // import { defineProps } from 'vue';
 const goods = ref([])
@@ -40,6 +41,7 @@ onMounted(()=>{
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
+               <ImageView></ImageView>
 
               <!-- 统计数量 -->
               <ul class="goods-sales">
